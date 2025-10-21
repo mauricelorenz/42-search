4b87aab566772577da2e0bd89a14bcbc3e4ebee2
Maurice Lorenz <me@mauricelorenz.com>
2025-10-21 12:13:14 +0200

    Rename variables and add blank lines for clarity

--- search_flask/flask_search.py
+++ search_flask/flask_search.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import requests
+# import urllib.parse
 
 app = Flask(__name__)
 
@@ -12,21 +13,27 @@ def result():
     with open("secret", "r") as f:
         cid = f.readline().strip()
         sec = f.readline().strip()
-    get_token_url = "https://api.intra.42.fr/oauth/token"
-    get_token_payload = f"grant_type=client_credentials&client_id={cid}&client_secret={sec}"
-    get_token_response = requests.post(get_token_url, data=get_token_payload)
-    token = get_token_response.json()["access_token"]
+
+    token_url = "https://api.intra.42.fr/oauth/token"
+    token_payload = f"grant_type=client_credentials&client_id={cid}&client_secret={sec}"
+    token_response = requests.post(token_url, data=token_payload)
+    token = token_response.json()["access_token"]
+
     campus = request.args["campus"].title()
-    get_campus_url = f"https://api.intra.42.fr/v2/campus?filter[name]={campus}"
+    # campus_encoded = urllib.parse.quote(campus)
+    campus_url = f"https://api.intra.42.fr/v2/campus?filter[name]={campus}"
     header = {"Authorization": f"Bearer {token}"}
-    get_campus_response = requests.get(get_campus_url, headers=header)
-    if not get_campus_response.json():
+    campus_response = requests.get(campus_url, headers=header)
+
+    if not campus_response.json():
         return render_template("42-result.html")
-    campus_id = get_campus_response.json()[0]["id"]
+    campus_id = campus_response.json()[0]["id"]
     first_name = request.args["first_name"]
-    get_users_url = f"https://api.intra.42.fr/v2/users?campus_id={campus_id}&filter[first_name]={first_name}"
-    get_users_reponse = requests.get(get_users_url, headers=header)
-    users = get_users_reponse.json()
+    # first_name_encoded = urllib.parse.quote(first_name)
+    users_url = f"https://api.intra.42.fr/v2/users?campus_id={campus_id}&filter[first_name]={first_name}"
+    users_response = requests.get(users_url, headers=header)
+
+    users = users_response.json()
     return render_template("42-result.html", users=users)
 
 if __name__ == '__main__':
